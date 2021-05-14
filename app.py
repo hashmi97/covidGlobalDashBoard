3e6ceb18b3d8ae17253e5c4d31086f9911601675
hashmi97 <h.m.hisham.alhashmi@gmail.com>
2021-05-14 02:45:11 -0700

    fixed deployment bug

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ def parse_date(date_time):
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+q
 colors = {
     'background': '#ffffff',
     'text': '#000080'
